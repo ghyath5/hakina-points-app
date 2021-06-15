e469cb2bf45b9e781a2286d8c8edb92c8129b213
Ghyath <ghyath.darwish@gmail.com>
2021-06-15 19:47:13 +0300

    search tab

--- src/screens/Tabs/Search.js
+++ src/screens/Tabs/Search.js
@@ -1,11 +1,53 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import theme from '../../theme'
-
+import { useQuery } from '@apollo/react-hooks'
+import React, { useMemo } from 'react'
+import { StyleSheet, FlatList, View, ActivityIndicator } from 'react-native'
+import Client from '../../components/Client'
+import { StatusBar } from 'expo-status-bar'
+import { PREVIOUS_CLIENTS } from '../../gql'
+import {
+    AdMobBanner, AdMobInterstitial,
+} from 'expo-ads-admob';
+import { mainInterstital, bannerRecId } from '../../Admob';
 export default function Search() {
+
+    React.useEffect(() => {
+        const bootstrapAsync = async () => {
+            await AdMobInterstitial.setAdUnitID(mainInterstital);
+            await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
+            await AdMobInterstitial.showAdAsync();
+        }
+        bootstrapAsync()
+    }, [])
+    const displayAd = async () => {
+        await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true });
+        await AdMobInterstitial.showAdAsync();
+    }
+    const { data, loading, error } = useQuery(PREVIOUS_CLIENTS, {})
+    let clients = data?.get_previous_clients?.clients
+    const renderItem = (props) => <Client {...props} onClick={() => {
+        if (Math.random() >= 0.4 && Math.random() >= 0.4) {
+            displayAd()
+        }
+    }} />
+    const memoRenderItem = useMemo(() => renderItem, [])
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 30, color: theme.primary }}>قادم قريباً</Text>
+        <View style={{ paddingTop: 10, height: '100%' }}>
+            <StatusBar style={'dark'} />
+            {loading &&
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator animating={loading} size={55} color={'blue'} />
+                </View>
+            }
+            <AdMobBanner
+                bannerSize="smartBannerPortrait"
+                adUnitID={bannerRecId}
+                servePersonalizedAds />
+            <FlatList
+                data={clients}
+                renderItem={memoRenderItem}
+                keyExtractor={(item) => item.tel_id}
+                contentContainerStyle={{ flexGrow: 1 }}
+            />
         </View>
     )
 }
