3b51df5cae064283816f7a1fbd27fd390550de09
Ghyath <ghyath.darwish@gmail.com>
2021-06-15 19:45:18 +0300

    add moment

--- src/Admob.js
+++ src/Admob.js
@@ -6,3 +6,12 @@ export const rewardedVidIdIOS = 'ca-app-pub-7749815556108724/7031470728'
 export const bannerRecIdIOS = 'ca-app-pub-7749815556108724/6895739354'
 
 export const mainInterstital = 'ca-app-pub-7749815556108724/3233015889'
+
+
+// export const bannerRecId = 'ca-app-pub-3940256099942544/6300978111'
+// export const rewardedVidId = 'ca-app-pub-3940256099942544/5224354917'
+
+// export const rewardedVidIdIOS = 'ca-app-pub-7749815556108724/7031470728'
+// export const bannerRecIdIOS = 'ca-app-pub-7749815556108724/6895739354'
+
+// export const mainInterstital = 'ca-app-pub-3940256099942544/1033173712'
