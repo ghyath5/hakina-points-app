e28e4cde59d48703d0ea999e858575b40cbb9f8c
Ghyath <ghyath.darwish@gmail.com>
2021-06-22 21:20:12 +0300

    update 4.2.0 version

--- src/screens/AppLoading.js
+++ src/screens/AppLoading.js
@@ -15,7 +15,7 @@ import Constants from 'expo-constants';
 export default function AppLoading(props) {
     const refreshToken = useSelector((state) => state?.client?.refreshToken)
     const dispatch = useDispatch()
-    if (Constants.nativeAppVersion != '4.1.4') {
+    if (Constants.nativeAppVersion != '4.2.0') {
         Alert.alert('تنبيه', "قم بتحديث التطبيق من المتجر", [
             {
                 text: 'تحديث',
